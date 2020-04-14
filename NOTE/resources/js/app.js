68b8fd4d52390b2cd3b521a3f57de25e8cc90f31
chobi1125 <chonmage112000@yahoo.co.jp>
2020-04-14 23:30:17 +0900

    note_2

---
+++ NOTE/resources/js/app.js
@@ -0,0 +1,10 @@
+import './bootstrap'
+import Vue from 'vue'
+import Example from './components/Example'
+
+const app = new Vue({
+el: '#app',
+  components: {
+    Example
+  }
+})
---
+++ NOTE/resources/views/layouts/app.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>Laravel</title>
+  <!-- sassのトランスパイル後のCSS読み込み -->
+  <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+</head>
+<body>
+<div id="app">
+  <p><example /></p>
+</div>
+</body>
+<script src="{{ mix('js/app.js') }}"></script>
